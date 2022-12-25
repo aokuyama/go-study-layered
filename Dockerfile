fd9864bcabc9ff4a184c175210f6b4474898bfe8
aokuyama <ao980s@gmail.com>
2022-12-25 15:28:21 +0900

    add gin

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,10 @@ RUN apt-get update && \
     go install -v golang.org/x/tools/gopls@latest && \
     go install -v github.com/stamblerre/gocode@latest && \
     go install -v github.com/rogpeppe/godef@latest
+
+WORKDIR /workspace
+
+COPY ./go.mod ./
+COPY ./go.sum ./
+
+RUN go mod download
--- main.go
+++ main.go
@@ -1,31 +1,11 @@
 package main
 
-import (
-	"github.com/aokuyama/go-study-layered/app/controller"
-	"github.com/aokuyama/go-study-layered/app/usecase/get_user_info"
-	"github.com/aokuyama/go-study-layered/domain/model/user"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	p := get_user_info.NewGetUserInfoPresenterCli()
-	r := UserRepositoryDummy{}
-	get_user_info := get_user_info.NewGetUserInfoInteractor(p, r)
-	c := controller.NewUserController(get_user_info)
-	// ユーザーが存在する場合
-	c.GetUserInfo("123456789")
-	// ユーザーが存在しない場合
-	c.GetUserInfo("12345678")
-	// エラー時
-	err := c.GetUserInfo("")
-	println(err.Error())
-}
-
-type UserRepositoryDummy struct {
-}
-
-func (r UserRepositoryDummy) LoadByID(id user.UserID) (*user.User, error) {
-	if id == "123456789" {
-		return user.New("太郎", "山田", "ok@example.com")
-	}
-	return nil, nil
+	r := gin.Default()
+	r.GET("/health_check", func(c *gin.Context) {
+		c.String(200, "OK")
+	})
+	r.Run()
 }
